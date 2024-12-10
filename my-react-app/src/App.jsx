f6dedbdf0c734b41d15f407027830a2fae90eef1
Thiago Hatadani <thiagohatadani@gmail.com>
2024-12-10 13:05:31 -0300

    Commit inicial - Navbar e Carousel

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Carousel from "./componentes/Carousel";
+import Navbar from "./componentes/Navbar";
+
+const App = () => {
+  return (
+    <html data-theme="light">
+      <Navbar />
+      <Carousel />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </html>
+  );
+};
+
+export default App;
